eb4fb067a785eda1c24bce2e04f17976c1a4b5f6
Alessia B <borys.alessia@gmail.com>
2022-11-28 18:30:34 +0100

    interface for character

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 import './App.css';
 import { useState, useEffect } from 'react';
 
-function App() {
-  const [state, setState] = useState()
+interface character {
+  films: [];
+  shortFilms: [];
+  tvShows: [];
+  videoGames: [];
+  parkAttractions: [];
+  allies: [];
+  enemies: [];
+  _id: number;
+  name: string;
+  imageUrl: string;
+  url: string;
+}
 
+function App() :JSX.ElementAttributesProperty {
+  const [state, setState] = useState<character>()
+
+  // API-main: https://disneyapi.dev/
+  // API-to-use: https://api.disneyapi.dev/characters
   // fetch an image to be displayed
   // fetch the name of the character and match
   // it to the input below,
