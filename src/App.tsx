ea2bd9d2d8ea1b4aadcaa9a701209c1574dc3cb6
Alessia B <borys.alessia@gmail.com>
2022-11-30 12:21:55 +0100

    rendering images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import './App.css';
 import { useState, useEffect } from 'react';
 
-interface character {
+interface Character {
   films: [];
   shortFilms: [];
   tvShows: [];
@@ -15,15 +14,30 @@ interface character {
   imageUrl: string;
   url: string;
 }
-
+interface Data {data:Character[]}
 // could add another quiz section, 
 // with every button PRESS another
 // section of the character is revealed
 // you get more points the earlier you guess
 // 1 minus point each time you guess it wrong
 
-function App() :JSX.ElementAttributesProperty {
-  const [state, setState] = useState<character>()
+function App() :JSX.Element {
+  const [data, setData] = useState<Data>()
+  const [char, setChar] = useState<Character>()
+  
+  useEffect(() => {
+    fetch('https://api.disneyapi.dev/characters')
+      .then(res => res.json())
+      .then(data => setData(data))
+    // this logs as undefined
+  }, []);
+
+  let charImg = ''
+  const handleGuess = () => {
+    setChar(data?.data[Math.floor(Math.random() * data?.data.length)])
+    console.log(char)
+    console.log(char?.imageUrl)
+  }
 
   // API-main: https://disneyapi.dev/
   // API-to-use: https://api.disneyapi.dev/characters
@@ -40,9 +54,20 @@ function App() :JSX.ElementAttributesProperty {
   // add to div with score of CORRECT_COUNT
   return (
     <div className="App">
-      <p>Who is this?</p>
+      <div>
+      {char && (
+        <>
+          <div className="character">
+            <p></p>
+            <img src={char.imageUrl} alt="disney character" className='character-img'/>
+          </div>
+        </>
+      )}
+      </div>
+      <img src={charImg} alt=""/>
+      <p>Who is it ^ ?</p>
       <input />
-      <button>Submit</button>
+      <button onClick={handleGuess}>Guess</button>
     </div>
   );
 }
